80644c58ce4881cefd2e9d462b2311e9823f6e49
Alex Hedley <alex.hedley@me.com>
2019-10-26 16:04:25 +0100

    Updated scores, created initial site

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,17 +1,27 @@
 var myApp = angular.module('myApp',[]);
 myApp.controller('myController', function ($scope, $http, $q, $filter) {
 
+    $scope.events = [];
+
     $scope.init = function () {
-        //getData();
+        getData();
     }
 
     getData = () =>  {
         var file = 'data/scores.json';
-        
+
         $http.get(file)
         .then(function(response) {
-            $scope.scores = response;
+            $scope.events = response.data.events;
         });
+    };
+
+    $scope.loadEvent = (event) => {
+        $scope.selectedEvent = event;
+    };
+
+    $scope.loadGame = (game) => {
+        $scope.selectedGame = game;
     };
 
     $scope.init();
